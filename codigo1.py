72a3377a8798e717f1787585fd47c674916e4582
shoutto74-ux <shoutto74@gmail.com>
2026-08-26 09:25:12 -0300

    codigo1.py

---
+++ codigo1.py
@@ -0,0 +1,62 @@
+#1 - Atividade
+
+nome = input ("Digite seu nome: ")
+print (f"Olá, {nome}! Seja bem vindo(a)! ")
+
+#2 - Atividade
+
+numero = int(input("Digite um número inteiro: "))
+dobro = numero *2
+print (f"O dobro é: {dobro}")
+
+#3 - Atividade
+
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+soma = numero1 + numero2
+print (f"A soma é: {soma}")
+
+#4 - Atividade
+
+idade = int(input("Digite sua idade: "))
+idade_futura = idade + 10
+print (f"Daqui a 10 anos, você terá : {idade_futura} anos")
+
+#5 - Atividade
+
+numero = float(input("Digite um número: "))
+quadrado = numero **2
+print (f"O quadrado é: {quadrado}")
+
+#6 - Atividade
+
+salario = float(input("Digite o salário: "))
+novo_salario = salario *1.10
+print (f"O novo salário com o aumento é: {novo_salario}")
+
+#7 - Atividade
+
+celsius = float(input("Digite a temperatura em Celsius: "))
+fahrenheit = (celsius *9/5) + 32
+print (f"A temperatura em Fahrenheit é: {fahrenheit}")
+
+#8 - Atividade
+
+numero = float(input("Digite um número: "))
+metade = numero / 2
+print (f"A metade do número é: {metade}")
+
+#9 - Atividade
+
+comprimento = float(input("Digite o comprimento do terreno: "))
+largura = float(input("Digite a largura do terreno: "))
+area = comprimento * largura
+print (f"A área do terreno é: {area}")
+
+#10 - Atividade
+
+numero = int(input("Digite um número: "))
+antecessor = numero -1
+sucessor = numero +1
+print (f"O antecessor é: {antecessor}")
+print (f"O sucessor é: {sucessor}")
